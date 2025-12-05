3a1a987d1869dfae9e3c32e23df12fe18c73ec2c
Arutyun <arutyundan@gmail.com>
2025-12-05 20:53:24 +0300

    добавил функцию get_date() в модуль widget

--- src/widget.py
+++ src/widget.py
@@ -16,19 +16,31 @@ def mask_account_card(user_data: str) -> str:
         return prefix + str(get_mask_account(int(numbers)))
     else:
         return "Неверно ввели номер"
-    return
 
 
-# list = [
-#     mask_account_card("Maestro 1596837868705199"),
-#     mask_account_card("Счет 64686473678894779589"),
-#     mask_account_card("MasterCard 7158300734726758"),
-#     mask_account_card("Счет 35383033474447895560"),
-#     mask_account_card("Visa Classic 6831982476737658"),
-#     mask_account_card("Visa Platinum 8990922113665229"),
-#     mask_account_card("Visa Gold 5999414228426353"),
-#     mask_account_card("Счет 73654108430135874305"),
-# ]
-#
-# for item in list:
-#     print(item)
+def get_date(date: str) -> str:
+    '''возвращает строку с датой в формате"ДД.ММ.ГГГГ"'''
+
+    YYYY = date[:4]
+    MM = date[5:7:]
+    DD = date[8:10:]
+    formatted_date = f'{DD}.{MM}.{YYYY}'
+    return (formatted_date)
+
+list = [
+    mask_account_card("Maestro 1596837868705199"),
+    mask_account_card("Счет 64686473678894779589"),
+    mask_account_card("MasterCard 7158300734726758"),
+    mask_account_card("Счет 35383033474447895560"),
+    mask_account_card("Visa Classic 6831982476737658"),
+    mask_account_card("Visa Platinum 8990922113665229"),
+    mask_account_card("Visa Gold 5999414228426353"),
+    mask_account_card("Счет 73654108430135874305"),
+]
+
+for item in list:
+    print(item)
+
+date = "2024-03-11T02:26:18.671407"
+print("\n" + get_date(date))
+

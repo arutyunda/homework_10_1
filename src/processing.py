650510caff596bfd8e9d570ee0a5c1fa46299453
Arutyun <arutyundan@gmail.com>
2026-01-06 13:34:40 +0300

    define sort_by_date()

--- src/processing.py
+++ src/processing.py
@@ -10,13 +10,32 @@ def filter_by_state(list_of_dicts: list, state: str = "EXECUTED") -> list:
     return filtered_list
 
 
-# проверка работы функции
-# dict = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+def sort_by_date(list_of_dicts: list, order: bool = True) -> list:
+    """принимает список словарей и необязательный параметр, задающий порядок сортировки (по умолчанию — убывание)
+    и возвращает новый список, отсортированный по дате"""
+
+    list_of_dicts = sorted(list_of_dicts, reverse=order, key=lambda list_of_dicts: list_of_dicts["date"])
+    return list_of_dicts
+
+
+# проверка работы функций
+
+# dict1 = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
 #         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
 #         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
 #         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 #
-# filtered_list = filter_by_state(dict, state ='CANCELED')
+# dict2 = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 #
-# for dict in filtered_list:
+# filtered_by_state = filter_by_state(dict1, state ='CANCELED')
+# for dict in filtered_by_state:
 #     print(f'{dict}')
+#
+# print()
+#
+# sorted_by_date = sort_by_date(dict2, order=False)
+# for dict in sorted_by_date:
+#     print(dict)
